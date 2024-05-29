f5ab96483a670d1fccaae44f6e185904cbbcb62c
Sehee Jang <seheejang94@gmail.com>
2024-05-29 10:19:55 +0900

    .

--- project/scripts/scripts.js
+++ project/scripts/scripts.js
@@ -1,3 +1,4 @@
+// ADD OPEN CLASS
 const hamButton = document.querySelector('#menu-toggle');
 const navigation = document.querySelector('.navigation');
 
@@ -5,3 +6,21 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+
+// ADD ACTIVE CLASS TO THE NAV WHEN CLICKED 
+document.addEventListener('DOMContentLoaded', function () {
+    const navigationLinks = document.querySelectorAll('.navigation a');
+
+    navigationLinks.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            const currentActive = document.querySelector('.navigation a.active');
+            if (currentActive) {
+                currentActive.classList.remove('active');
+            }
+            link.classList.add('active');
+            console.log('Link clicked');
+        });
+    });
+});
+
